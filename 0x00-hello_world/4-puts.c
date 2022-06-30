622e5abce14f7bb61be5d8e3987c65353634ab46
Crowncare <106904694+Crowncare@users.noreply.github.com>
2022-06-30 11:01:05 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-
-
-
-
-
-
-
-
+#include <stdio.h>
+/**
+ * main - print the strings that inputs the function
+ * Description:printing"Programming is like building a multilingual puzzle"
+ * Return:0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

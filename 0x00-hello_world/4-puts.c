e1efff00e9914f5da7b06f281c015fc20a73a75e
Crowncare <106904694+Crowncare@users.noreply.github.com>
2022-06-30 07:34:30 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-printf (" Programming is like building a multilingual puzzle ")
+#include <stdio.h>
+
+int nain(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+return 0();
+}
